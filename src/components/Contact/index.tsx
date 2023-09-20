d2e76152258ef091985756d77f87b7a2df629b4c
faithandjs <okogbofaith@email.com>
2023-09-20 01:21:54 +0100

    changing contact

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -11,14 +11,14 @@ export default function Index() {
   return (
     <section className='contact' id='contact'>
       <h3>Contact me</h3>
-      <ul>
+      {/* <ul>
         <li>
           <a
             href='mailto:okogbofaith@gmail.com'
             target='_blank'
             rel='noreferrer'>
             <img src={email} alt='email icon' />
-            <span>okogbofaith@gmail.com</span>
+
           </a>
         </li>
         <li>
@@ -42,10 +42,57 @@ export default function Index() {
             target='_blank'
             rel='noreferrer'>
             <img src={githubImg} alt='github logo' />
-            <span>faithandjs</span>
+           
           </a>
         </li>
-      </ul>
+      </ul> */}
+
+      <div className='btn-box'>
+        <button className='btn'>
+          <a
+            href='mailto:okogbofaith@gmail.com'
+            target='_blank'
+            rel='noreferrer'>
+            <span className='details'>
+              <span> Email</span> <span>okogbofaith@gmail.com</span>
+            </span>
+
+            <img src={email} alt='email icon' />
+          </a>
+        </button>
+        <button className='btn'>
+          <a href='tel:+2348172994910' target='_blank' rel='noreferrer'>
+            <span className='details'>
+              <span> Phone</span>
+              <span>+2348172994910</span>
+            </span>
+            <img src={phone} alt='phone icon' />
+          </a>
+        </button>
+        <button className='btn'>
+          <a
+            href='https://www.linkedin.com/in/okogbo-faith'
+            target='_blank'
+            rel='noreferrer'>
+            <span className='details'>
+              <span> LinkedIn</span> <span>Okogbo Faith</span>
+            </span>
+            <img src={linkedin} alt='linkedin logo' />
+          </a>
+        </button>
+        <button className='btn'>
+          <a
+            href='https://github.com/faithandjs'
+            target='_blank'
+            rel='noreferrer'>
+            <span className='details'>
+              <span> Github</span>
+              <span>faithandjs</span>
+            </span>
+            <img src={githubImg} alt='github logo' />
+          </a>
+        </button>
+      </div>
     </section>
   );
 }
